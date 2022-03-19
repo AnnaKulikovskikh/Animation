4d59387bd74b9a551b3710f0c9c56c55991eff5b
Anna <k_anna_i@mail.ru>
2022-03-19 23:15:36 +0300

    linter

--- src/js/liker.js
+++ src/js/liker.js
@@ -2,13 +2,13 @@ export default function liker() {
   const like = document.querySelector('.like');
   let s = 0;
   like.addEventListener('click', ({ target }) => {
-    s++;
+    s += 1;
     const fly = document.createElement('div');
     fly.className = 'fly';
     if (s % 2 === 0) {
-        fly.classList.add('fly_left');
+      fly.classList.add('fly_left');
     } else {
-        fly.classList.add('fly_right');
+      fly.classList.add('fly_right');
     }
     fly.style.top = `${target.offsetTop - fly.offsetHeight}px`;
     fly.style.left = `${target.offsetWidth + target.offsetLeft - fly.offsetWidt / 2}px`;

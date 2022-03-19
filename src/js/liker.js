405153e600be713165ea0fe3723b057c6399ccec
Anna <k_anna_i@mail.ru>
2022-03-19 23:14:18 +0300

    like improve

--- src/js/liker.js
+++ src/js/liker.js
@@ -1,8 +1,15 @@
 export default function liker() {
   const like = document.querySelector('.like');
+  let s = 0;
   like.addEventListener('click', ({ target }) => {
+    s++;
     const fly = document.createElement('div');
     fly.className = 'fly';
+    if (s % 2 === 0) {
+        fly.classList.add('fly_left');
+    } else {
+        fly.classList.add('fly_right');
+    }
     fly.style.top = `${target.offsetTop - fly.offsetHeight}px`;
     fly.style.left = `${target.offsetWidth + target.offsetLeft - fly.offsetWidt / 2}px`;
     target.append(fly);
